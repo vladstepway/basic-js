fd632d15eb0be690c24080ebbe56b73096683f73
ustsepavy <vladstepovoyvios@gmail.com>
2020-09-07 21:10:51 +0300

    feat:resolve carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,13 @@
-const CustomError = require("../extensions/custom-error");
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const RADIOACTIVE_DECAY = 0.693 / HALF_LIFE_PERIOD;
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
-
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    return (typeof sampleActivity !== 'string'
+        || /[a-zA-Z]/.test(sampleActivity)
+        || sampleActivity > MODERN_ACTIVITY
+        || sampleActivity <= 0)
+        ? false
+        : Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / RADIOACTIVE_DECAY);
 };
+
