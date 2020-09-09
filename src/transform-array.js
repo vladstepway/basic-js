0fb236b2f32de7c95c33d7b613bac170c0a9636b
ustsepavy <vladstepovoyvios@gmail.com>
2020-09-09 20:42:44 +0300

    feat: transform array task resolved

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,28 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error();
+    }
+    const copyArray = [...arr];
+    copyArray.map((cur, index, array) => {
+        if (cur === '--double-next') {
+            index !== array.length ? (array[index] = array[index + 1]) : false;
+        } else if (cur === '--double-prev') {
+            index - 1 > 0 ? (array[index] = array[index - 1]) : false;
+        } else if (cur === '--discard-next') {
+            index !== array.length ? (array[index + 1] = undefined) : false;
+        } else if (cur === '--discard-prev') {
+            index - 1 ? (array[index - 1] = undefined) : false;
+        }
+    });
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+    return copyArray.filter(
+        (x) =>
+            x !== undefined &&
+            [
+                '--discard-next',
+                '--discard-prev',
+                '--double-prev',
+                '--double-prev',
+            ].indexOf(x) === -1
+    );
+}
